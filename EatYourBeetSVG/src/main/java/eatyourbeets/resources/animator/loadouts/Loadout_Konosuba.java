255be2719ca55b103656cca253856f9213f579e3
Touhou <122487411+leo-in-the-house@users.noreply.github.com>
2023-10-19 13:42:51 -0500

    Loadout for Konosuba updated

--- EatYourBeetSVG/src/main/java/eatyourbeets/resources/animator/loadouts/Loadout_Konosuba.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/resources/animator/loadouts/Loadout_Konosuba.java
@@ -1,8 +1,6 @@
 package eatyourbeets.resources.animator.loadouts;
 
-import eatyourbeets.cards.animator.curse.common.Curse_Greed;
 import eatyourbeets.cards.animator.series.Konosuba.*;
-import eatyourbeets.cards.animator.status.Status_Slimed;
 import eatyourbeets.cards.animator.ultrarare.Chomusuke;
 import eatyourbeets.cards.base.CardSeries;
 import eatyourbeets.cards.base.EYBCardData;
@@ -21,22 +19,12 @@ public class Loadout_Konosuba extends AnimatorLoadout
     @Override
     public void AddStarterCards()
     {
-        AddStarterCard(Mitsurugi.DATA, 6);
-        AddStarterCard(Kazuma.DATA, 7);
-        AddStarterCard(Verdia.DATA, 8);
+        AddStarterCard(Mitsurugi.DATA, 8);
+        AddStarterCard(Kazuma.DATA, 5);
+        AddStarterCard(Verdia.DATA, 6);
         AddStarterCard(Vanir.DATA, 8);
-        AddStarterCard(Sylvia.DATA, 8);
-
-        AddStarterCard(YunYun.DATA, 9);
-        AddStarterCard(Chris.DATA, 6);
-        AddStarterCard(Megumin.DATA, 14);
-        AddStarterCard(Aqua.DATA, 6);
-
-        AddStarterCard(Wiz.DATA, 22);
-        AddStarterCard(Eris.DATA, 24);
-
-        AddStarterCard(Status_Slimed.DATA, -5);
-        AddStarterCard(Curse_Greed.DATA, -8);
+        AddStarterCard(Chris.DATA, 7);
+        AddStarterCard(YunYun.DATA, 7);
     }
 
     @Override
