ea7017512a01e74acd5c4b05135c7cdaca55c6eb
Touhou <122487411+leo-in-the-house@users.noreply.github.com>
2023-11-06 19:05:26 -0600

    Swap Ciel and Chung

--- EatYourBeetSVG/src/main/java/eatyourbeets/resources/animator/loadouts/Loadout_Elsword.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/resources/animator/loadouts/Loadout_Elsword.java
@@ -17,8 +17,8 @@ public class Loadout_Elsword extends AnimatorLoadout
     public void AddStarterCards()
     {
         AddStarterCard(Elsword.DATA, 7);
-        AddStarterCard(Ciel.DATA, 6);
-        AddStarterCard(Chung.DATA, 5);
+        AddStarterCard(Ciel.DATA, 5);
+        AddStarterCard(Chung.DATA, 6);
         AddStarterCard(Raven.DATA, 8);
         AddStarterCard(Ara.DATA, 7);
     }
