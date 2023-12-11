3019ebc06452501c0619b525cb0df5315e9346f0
Touhou <122487411+leo-in-the-house@users.noreply.github.com>
2023-12-11 09:26:34 -0600

    Add guaranteed colorless for merchants fixed

--- EatYourBeetSVG/src/main/java/patches/merchant/MerchantPatches.java
+++ EatYourBeetSVG/src/main/java/patches/merchant/MerchantPatches.java
@@ -5,7 +5,9 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.shop.Merchant;
 import eatyourbeets.cards.base.EYBCard;
 import eatyourbeets.interfaces.listeners.OnAddingToCardRewardListener;
@@ -13,6 +15,7 @@ import eatyourbeets.utilities.JUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class MerchantPatches
 {
@@ -85,18 +88,34 @@ public class MerchantPatches
             AbstractDungeon.commonCardPool = data.common;
             AbstractDungeon.uncommonCardPool = data.uncommon;
             AbstractDungeon.rareCardPool = data.rare;
-
             //Prevent this from being overriden with even mods
+            float drawStartX = (float)Settings.WIDTH * 0.16F;
+            int tmp = (int)((float)Settings.WIDTH - drawStartX * 2.0F - AbstractCard.IMG_WIDTH_S * 5.0F) / 4;
+            float padX = (float)((int)((float)tmp + AbstractCard.IMG_WIDTH_S));
+
+            AbstractDungeon.shopScreen.colorlessCards.clear();
+
             ___cards2.clear();
-            ___cards2.add(AbstractDungeon.getColorlessCardFromPool(AbstractCard.CardRarity.UNCOMMON).makeCopy());
-            ___cards2.add(AbstractDungeon.getColorlessCardFromPool(AbstractCard.CardRarity.RARE).makeCopy());
 
-            ___cards2.get(0).price = 50;
-            ___cards2.get(1).price = 100;
+            for (int i=0; i<2; i++)
+            {
+                AbstractCard card = AbstractDungeon.getColorlessCardFromPool(i == 0 ? AbstractCard.CardRarity.UNCOMMON : AbstractCard.CardRarity.RARE).makeCopy();
+                card.price = i == 0 ? 30 : 60;
+                card.updateHoverLogic();
+                card.targetDrawScale = 0.75F;
+                card.current_x = drawStartX + AbstractCard.IMG_WIDTH_S / 2.0F + padX * (float)i;
+                card.target_x = drawStartX + AbstractCard.IMG_WIDTH_S / 2.0F + padX * (float)i;
+                card.target_y = 9999.0F * Settings.scale;
+                card.current_y = 9999.0F * Settings.scale;
 
+                Iterator var6 = AbstractDungeon.player.relics.iterator();
 
-            AbstractDungeon.shopScreen.colorlessCards.addAll(___cards2);
-
+                while(var6.hasNext()) {
+                    AbstractRelic r = (AbstractRelic)var6.next();
+                    r.onPreviewObtainCard(card);
+                }
+                ___cards2.add(card);
+            }
         }
     }
 }
