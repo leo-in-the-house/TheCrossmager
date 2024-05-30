b68ef51a162752ec19be55332f4c3c1ae8dea6d2
Touhou <122487411+leo-in-the-house@users.noreply.github.com>
2024-05-30 08:57:35 -0500

    Adding new colorless

---
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/colorless/rare/ArseneLupinIII.java
@@ -0,0 +1,59 @@
+package eatyourbeets.cards.animator.colorless.rare;
+
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.RainingGoldEffect;
+import eatyourbeets.cards.base.AnimatorCard;
+import eatyourbeets.cards.base.CardUseInfo;
+import eatyourbeets.cards.base.EYBCardData;
+import eatyourbeets.cards.base.EYBCardTarget;
+import eatyourbeets.effects.AttackEffects;
+import eatyourbeets.effects.VFX;
+import eatyourbeets.utilities.GameActions;
+import eatyourbeets.utilities.GameEffects;
+import eatyourbeets.utilities.GameUtilities;
+
+public class ArseneLupinIII extends AnimatorCard {
+    public static final EYBCardData DATA = Register(ArseneLupinIII.class)
+            .SetSkill(2, CardRarity.RARE, EYBCardTarget.None)
+            .SetColor(CardColor.COLORLESS)
+            .SetSeriesFromClassPackage();
+
+    public ArseneLupinIII() {
+        super(DATA);
+
+        Initialize(0, 20, 5, 10);
+        SetUpgrade(0, 0, 4, 5);
+        SetAffinity_Yellow(2, 0, 1);
+
+        SetPurge(true);
+        SetHaste(true);
+    }
+
+    @Override
+    public void OnUse(AbstractPlayer p, AbstractMonster m, CardUseInfo info) {
+        GameUtilities.PlayVoiceSFX(name);
+
+        GameActions.Bottom.GainBlock(block);
+
+        GameActions.Bottom.Reload(name, cards ->
+        {
+            if (cards.size() > 0) {
+                GameEffects.Queue.Add(new RainingGoldEffect(secondaryValue * cards.size()));
+                for (int i=0; i<cards.size(); i++)
+                {
+                    GameActions.Top.Wait(0.2f);
+                    GameActions.Top.GainGold(secondaryValue);
+                    GameActions.Top.DealDamageToRandomEnemy(magicNumber, DamageInfo.DamageType.THORNS, AttackEffects.NONE)
+                            .SetOptions(true, false)
+                            .SetDamageEffect(c ->
+                                    GameEffects.List.Add(VFX.ThrowDagger(c.hb, 0.15f).SetColor(Color.WHITE))
+                                            .duration * 0.25f);
+
+                }
+            }
+        });
+    }
+}
---
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/colorless/rare/HatsuneMiku.java
@@ -0,0 +1,93 @@
+package eatyourbeets.cards.animator.colorless.rare;
+
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.PetalEffect;
+import com.megacrit.cardcrawl.vfx.RainbowCardEffect;
+import eatyourbeets.cards.base.AnimatorCard;
+import eatyourbeets.cards.base.CardUseInfo;
+import eatyourbeets.cards.base.EYBCardData;
+import eatyourbeets.powers.AnimatorPower;
+import eatyourbeets.utilities.GameActions;
+import eatyourbeets.utilities.GameEffects;
+import eatyourbeets.utilities.GameUtilities;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class HatsuneMiku extends AnimatorCard {
+    public static final EYBCardData DATA = Register(HatsuneMiku.class)
+            .SetPower(0, CardRarity.RARE)
+            .SetColor(CardColor.COLORLESS)
+            .SetSeriesFromClassPackage();
+
+    public HatsuneMiku() {
+        super(DATA);
+
+        Initialize(0, 0, 0);
+        SetUpgrade(0, 0, 0);
+
+        SetEthereal(true);
+    }
+
+    @Override
+    protected void OnUpgrade()
+    {
+        SetInnate(true);
+    }
+
+
+    @Override
+    public boolean cardPlayable(AbstractMonster m)
+    {
+        if (super.cardPlayable(m))
+        {
+            return player.masterDeck.size() == 39;
+        }
+
+        return false;
+    }
+
+    @Override
+    public void OnUse(AbstractPlayer p, AbstractMonster m, CardUseInfo info) {
+        GameUtilities.PlayVoiceSFX(name);
+        GameEffects.Queue.BorderFlash(Color.SKY);
+        GameEffects.Queue.Add(new PetalEffect());
+        GameEffects.Queue.Add(new RainbowCardEffect());
+
+        List<AbstractCard> upgradableCards = new LinkedList<>();
+        upgradableCards.addAll(player.drawPile.group);
+        upgradableCards.addAll(player.hand.group);
+        upgradableCards.addAll(player.discardPile.group);
+
+        for (AbstractCard card : upgradableCards) {
+            if (card.canUpgrade()) {
+                card.upgrade();
+                card.applyPowers();
+            }
+        }
+
+        GameActions.Bottom.StackPower(new HatsuneMikuPower(p, 1));
+    }
+
+    public static class HatsuneMikuPower extends AnimatorPower {
+        public HatsuneMikuPower(AbstractCreature owner, int amount) {
+            super(owner, HatsuneMiku.DATA);
+            Initialize(amount);
+        }
+
+        @Override
+        public void updateDescription() {
+            description = FormatDescription(0, amount);
+        }
+
+        @Override
+        public void onCardDraw(AbstractCard c)
+        {
+            c.setCostForTurn(-9);
+        }
+    }
+}
