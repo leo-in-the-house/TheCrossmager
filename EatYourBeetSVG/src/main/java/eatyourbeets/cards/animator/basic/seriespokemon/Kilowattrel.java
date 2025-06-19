5b2e757e85aebe10f4e68908ded6769d299d6d85
Touhou <122487411+leo-in-the-house@users.noreply.github.com>
2025-06-18 19:32:36 -0500

    Adding Blue Archive Pokemon

---
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/basic/seriespokemon/Kilowattrel.java
@@ -0,0 +1,41 @@
+package eatyourbeets.cards.animator.basic.seriespokemon;
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.orbs.Lightning;
+import eatyourbeets.cards.animator.basic.pokemon.PokemonCard;
+import eatyourbeets.cards.base.CardUseInfo;
+import eatyourbeets.cards.base.EYBCardData;
+import eatyourbeets.cards.base.EYBCardTarget;
+import eatyourbeets.utilities.GameActions;
+import eatyourbeets.utilities.GameUtilities;
+
+public class Kilowattrel extends PokemonCard {
+    public static final EYBCardData DATA = Register(Kilowattrel.class)
+            .SetSkill(1, CardRarity.BASIC, EYBCardTarget.Normal);
+
+    public Kilowattrel() {
+        super(DATA);
+
+        Initialize(0, 0, 3);
+        SetUpgrade(0, 0, 0);
+    }
+
+    @Override
+    protected void OnUpgrade()
+    {
+        super.OnUpgrade();
+
+        SetRetain(true);
+    }
+
+    @Override
+    public void OnUse(AbstractPlayer p, AbstractMonster m, CardUseInfo info) {
+        GameUtilities.PlayVoiceSFX(name);
+
+        for (int i=0; i<magicNumber; i++) {
+            GameActions.Bottom.ChannelOrb(new Lightning());
+        }
+        GameActions.Bottom.ApplyLockOn(p, m, magicNumber);
+    }
+}
