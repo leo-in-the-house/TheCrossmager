261c7cdf075f2e6ce63d7aed25ec2d457833178e
Touhou <122487411+leo-in-the-house@users.noreply.github.com>
2025-06-21 19:11:24 -0500

    Arona Redesign

--- EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/special/Arona.java
+++ EatYourBeetSVG/src/main/java/eatyourbeets/cards/animator/special/Arona.java
@@ -1,24 +1,22 @@
 package eatyourbeets.cards.animator.special;
 
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import eatyourbeets.cards.base.*;
 import eatyourbeets.utilities.GameActions;
 import eatyourbeets.utilities.GameUtilities;
-
-import java.util.HashMap;
+import eatyourbeets.utilities.TargetHelper;
 
 public class Arona extends AnimatorCard {
     public static final EYBCardData DATA = Register(Arona.class)
-            .SetSkill(0, CardRarity.SPECIAL, EYBCardTarget.None)
+            .SetSkill(0, CardRarity.SPECIAL, EYBCardTarget.Normal)
             .SetSeries(CardSeries.BlueArchive);
 
     public Arona() {
         super(DATA);
 
-        Initialize(0, 0, 2);
-        SetUpgrade(0, 0, 1);
+        Initialize(0, 0, 2, 2);
+        SetUpgrade(0, 0, 1, 1);
 
         SetAffinity_Teal(1);
     }
@@ -29,24 +27,6 @@ public class Arona extends AnimatorCard {
 
         GameActions.Bottom.GainTeal(magicNumber);
 
-        HashMap<String, AnimatorCard> abydosStudents = GameUtilities.GetAbydosStudents();
-
-        for (AbstractCard card : player.hand.group) {
-            boolean marked = false;
-
-            if (upgraded && GameUtilities.IsSeries(card, CardSeries.BlueArchive)) {
-                marked = true;
-            }
-            else if (!upgraded) {
-                if (abydosStudents.get(card.cardID) != null)
-                {
-                    marked = true;
-                }
-            }
-
-            if (marked) {
-                GameActions.Top.IncreaseScaling(card, Affinity.Teal, 1);
-            }
-        }
+        GameActions.Bottom.ApplyLockOn(TargetHelper.Normal(m), secondaryValue);
     }
 }
